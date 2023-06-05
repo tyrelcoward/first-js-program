26ec9d0f6cf8598a3c832d6a9fedadd102cda488
PonosDev <ponosdev@proton.me>
2023-06-05 01:51:02 -0400

    Add variables storing selections

--- myfile.js
+++ myfile.js
@@ -1,3 +1,6 @@
+const playerSelection = prompt("What is your choice?");
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
     const choices = ['rock','paper', 'scissors'];
     random =  Math.floor(Math.random() * choices.length);
