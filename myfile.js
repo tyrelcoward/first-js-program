6f9fb31c7b18d71ddcd612ff2c177215a9f8c94c
PonosDev <ponosdev@proton.me>
2023-06-05 03:07:35 -0400

    Add final game function

--- myfile.js
+++ myfile.js
@@ -1,27 +1,58 @@
-const playerSelection = prompt("What is your choice?").toLowerCase();
-const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     const choices = ['rock','paper', 'scissors'];
-    random =  Math.floor(Math.random() * choices.length);
-    choice = choices[random];
+    let random =  Math.floor(Math.random() * choices.length);
+    let choice = choices[random];
     return choice;
 }
 
+
 function play(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
-        console.log("Tie! You both selected" + playerSelection);
+         console.log("Tie! You both selected " + playerSelection);
     }else if (playerSelection == 'rock' && computerSelection == 'scissors'){
-        console.log("You win, Rock beats Scissors!");
+         console.log("You win, Rock beats Scissors!");
+         return true;
     }else if (playerSelection == 'rock' && computerSelection == 'paper'){
-        console.log("You lost! Paper beats Rock!")
+         console.log("You lost! Paper beats Rock!");
+         return false;
     }else if (playerSelection == 'paper' && computerSelection == 'rock'){
-        console.log("You win! Paper beats Rock!")
+         console.log("You win! Paper beats Rock!");
+         return true;
     }else if (playerSelection == 'paper' && computerSelection == 'scissors'){
-        console.log("You lose! Scissors beats Paper!")
+         console.log("You lose! Scissors beats Paper!");
+         return true;
     }else if (playerSelection == 'scissors' && computerSelection == 'rock'){
-        console.log("You lose! Rock beats Scissors!")
+         console.log("You lose! Rock beats Scissors!");
+         return false;
     }else if (playerSelection == 'scissors' && computerSelection == 'paper'){
-        console.log("You win! Scissors beats paper!")
+         console.log("You win! Scissors beats paper!");
+         return true;
     }
 }
+
+function game(){
+    var userWins = 0;
+    var compWins = 0;
+    let i = 0;
+    while (i < 5) { 
+         let playerSelection = prompt("What is your choice?").toLowerCase();
+         let computerSelection = getComputerChoice();
+         if (play(playerSelection, computerSelection)) {
+            userWins++;
+         }else {
+            compWins++;
+         }
+         i++;
+     }
+     if (userWins == compWins){
+        console.log("There has been a tie!");
+     }else if (userWins < compWins){
+        console.log("The computer has won!");
+     }else {
+        console.log("You have won");
+     }
+
+}
+
+console.log(game());
