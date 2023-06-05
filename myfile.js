7e99b7003bc355509970e94e2472a72bc064e9c5
PonosDev <ponosdev@proton.me>
2023-06-05 01:55:54 -0400

    Add play function

--- myfile.js
+++ myfile.js
@@ -7,3 +7,21 @@ function getComputerChoice() {
     choice = choices[random];
     return choice;
 }
+
+function play(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        console.log("Tie! You both selected" + playerSelection);
+    }else if (playerSelection == 'rock' && computerSelection == 'scissors'){
+        console.log("You win, Rock beats Scissors!");
+    }else if (playerSelection == 'rock' && computerSelection == 'paper'){
+        console.log("You lost! Paper beats Rock!")
+    }else if (playerSelection == 'paper' && computerSelection == 'rock'){
+        console.log("You win! Paper beats Rock!")
+    }else if (playerSelection == 'paper' && computerSelection == 'scissors'){
+        console.log("You lose! Scissors beats Paper!")
+    }else if (playerSelection == 'scissors' && computerSelection == 'rock'){
+        console.log("You lose! Rock beats Scissors!")
+    }else if (playerSelection == 'scissors' && computerSelection == 'paper'){
+        console.log("You win! Scissors beats paper!")
+    }
+}
